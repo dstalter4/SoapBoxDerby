459629dc05c66ed3fbb6696df83ad06b01d1fdae
David Stalter <dstalter4@gmail.com>
2018-07-08 19:49:21 -0400

    Add IsCarDataRequested().  Only transmit data once per second.  Support for transmitting raw and formatted data.

--- SoapBoxDerbyCar/SerialPort.ino
+++ SoapBoxDerbyCar/SerialPort.ino
@@ -47,10 +47,11 @@ void SoapBoxDerbyCar::ConfigureSerialPorts()
   // Enable the default UART for printing
   const long int SERIAL_PORT_BAUD_RATE = 115200;
   Serial.begin(SERIAL_PORT_BAUD_RATE);
-  Serial.println("Initializing...");
+  Serial.println(F("Initializing..."));
 
   // Enable the serial port for transmitting car data
   m_pDataTransmitSerialPort->begin(SERIAL_PORT_BAUD_RATE);
+  m_pDataTransmitSerialPort->setTimeout(SERIAL_PORT_TIMEOUT_MS);
 }
 
 
@@ -66,6 +67,27 @@ bool SoapBoxDerbyCar::IsSerialTransmitSwitchSet()
 
 
 ////////////////////////////////////////////////////////////////////////////////
+/// Method: IsCarDataRequested
+///
+/// Details:  Checks if there is a request to send serial data.
+////////////////////////////////////////////////////////////////////////////////
+bool SoapBoxDerbyCar::IsCarDataRequested()
+{
+  bool bResult = false;
+  
+  if (m_pDataTransmitSerialPort->available())
+  {
+    if (m_pDataTransmitSerialPort->readString() == SERIAL_PORT_DATA_REQUEST_STRING)
+    {
+      bResult = true;
+    }
+  }
+
+  return bResult;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
 /// Method: SendCarSerialData
 ///
 /// Details:  Sends basic information about the state of the car and its sensors
@@ -73,12 +95,21 @@ bool SoapBoxDerbyCar::IsSerialTransmitSwitchSet()
 ////////////////////////////////////////////////////////////////////////////////
 void SoapBoxDerbyCar::SendCarSerialData()
 {
+  static int transmitCount = 0;
+  static unsigned long lastTimeStamp = 0;
+  
   const int NUM_FIELDS_TO_TRANSMIT = 13;
-  int transmitDataIndex = 0;
   int32_t serialData[NUM_FIELDS_TO_TRANSMIT] = {};
   static_assert(sizeof(serialData) == NUM_FIELDS_TO_TRANSMIT * 4, "Serial data size is not using 32 bit integers!");
+
+  // If enough time hasn't elapsed, don't transmit
+  if (CalcDeltaTimeMs(lastTimeStamp) < SERIAL_DATA_TRANSMIT_INTERVAL_MS)
+  {
+    return;
+  }
   
   // Package together all of the data
+  int transmitDataIndex = 0;
   serialData[transmitDataIndex++] = m_CurrentSteeringValue;
   serialData[transmitDataIndex++] = m_bReleasingBrake;
   serialData[transmitDataIndex++] = m_bApplyingBrake;
@@ -96,25 +127,42 @@ void SoapBoxDerbyCar::SendCarSerialData()
   // Make sure the buffer wasn't overrun
   ASSERT(transmitDataIndex == NUM_FIELDS_TO_TRANSMIT);
 
+  if (DEBUG_PRINTS)
+  {
+    Serial.print(F("Serial data transmission #"));
+    Serial.println(++transmitCount);
+  }
+
   // The preferred way to transmit is by writing out via a byte pointer.
   // However, the listener on the other end could be something like
   // javascript, which does not provide easy direct support of manipulating
-  // byte streams.  As such, println() is leveraged here instead of write().
-  // If the receiver has better support of byte manipulation and conversion,
-  // the stream can be written directly instead.  For example:
-  // 
-  // Get a pointer to the data for use when writing out to the serial port
-  //byte * pData = reinterpret_cast<byte *>(&serialData);
-  // In a for loop:
-  //m_pDataTransmitSerialPort->write(*pData++);
+  // byte streams.  If the receiver has better support of byte manipulation
+  // and conversion, the stream can be written directly instead.  Configure
+  // this via the SERIAL_PORT_USE_RAW_DATA variable.
 
   // Send the header
   m_pDataTransmitSerialPort->println("SBDC");
 
-  // Send the data
-  for (size_t i = 0; i < NUM_FIELDS_TO_TRANSMIT; i++)
+  if (SERIAL_PORT_USE_RAW_DATA)
   {
-    m_pDataTransmitSerialPort->println(serialData[i]);
+    byte * pData = reinterpret_cast<byte *>(&serialData);
+    for (size_t i = 0; i < sizeof(serialData); i++)
+    {
+      m_pDataTransmitSerialPort->write(*pData++);
+    }
+
+    // Send a newline footer to indicate the end of the data
+    m_pDataTransmitSerialPort->println();
   }
+  else
+  {
+    // Send the data
+    for (size_t i = 0; i < NUM_FIELDS_TO_TRANSMIT; i++)
+    {
+      m_pDataTransmitSerialPort->println(serialData[i]);
+    }
+  }
+
+  lastTimeStamp = GetTimeStampMs();
 }
 
